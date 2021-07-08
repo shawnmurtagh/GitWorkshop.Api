91e3a029e893371e1196eee3c5581b2ef27aabd1
Shawn Murtagh <smurtagh@foundationsoft.com>
2021-07-08 07:56:02 -0400

    init node server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GitWorkshop.Api
+
+## CLI
+
+you'll need to install nodejs and run
+
+`npm i express`
+`npm server`
+
+Then you should be able to view your server on [http://localhost:5000/](http://localhost:5000/)
--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 });
+
+app.get('/:name', (req, res) => {
+    res.send(`<h1>Welcome to ${req.params.name}!</h1>`);
+});
+
+app.listen(5000, console.log('Server is running on port 5000'));
